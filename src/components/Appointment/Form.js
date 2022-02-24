b2d4107f7562ff7da00b30e7b725c7a383d56e6b
Varsha Panchal <varsha3051@gmail.com>
2022-02-24 12:52:38 -0500

    add static Schedule for Appointment

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react';
+import InterviewerList from '../InterviewerList';
+import Button from '../Button';
+
+const Form = (props) => {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset=()=> {
+    setStudent("")
+    setInterviewer(null)
+  }
+
+  const cancel=()=> {
+    reset()
+    props.onCancel()
+  }
+return (
+<main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form onSubmit={event => event.preventDefault()} autoComplete="off" >
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        value ={student}
+        onChange={(event)=> setStudent(event.target.value)}
+      />
+    </form>
+    <InterviewerList 
+      /* your code goes here */
+       value={interviewer}
+        onChange={setInterviewer}
+        interviewers={props.interviewers}
+    />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={cancel}>Cancel</Button>
+      <Button confirm onClick={()=>{props.onSave(student, interviewer)}}>Save</Button>
+    </section>
+  </section>
+</main>
+)
+
+}
+
+export default Form;
